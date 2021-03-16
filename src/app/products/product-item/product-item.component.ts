e6d3886a9a9d0e1e324f13b59b3d9789b15c73cf
Jovic92 <milan.jovic@dmi.uns.ac.rs>
2021-03-16 13:52:14 +0100

    last commit

--- src/app/products/product-item/product-item.component.ts
+++ src/app/products/product-item/product-item.component.ts
@@ -10,16 +10,27 @@ export class ProductItemComponent implements OnInit {
 
   public product: Product;
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void {
     this.product = {
       name: "Monitor Dell",
       price: 25000,
       isOnSale: true,
-      quantity:0,
-      imageUrl:"https://img.gigatron.rs/img/products/large/image5b854e7ad91cf.png"
+      quantity: 0,
+      imageUrl: "https://img.gigatron.rs/img/products/large/image5b854e7ad91cf.png"
     }
   }
 
+  
+  incrementInCart() {
+    this.product.quantity++;
+  }
+
+
+  decrementInCart() {
+    if (this.product.quantity > 0) {
+      this.product.quantity--;
+    }
+  }
 }
